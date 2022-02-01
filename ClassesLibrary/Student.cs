3af4b811beb7c908b960d70b42b89c7d1755b07a
Romeo Bagunu <romeo.bagunu@outlook.com>
2022-02-01 16:37:04 -0600

    Completed the "Student" lab set. Student.cs has fields and properties for firstName (string), lastName (string), id (string), and gpa (float). It also has both a parameterless constructor and a FQCTR. It overrides ToString() to print each student's First Name, Last Name, ID, and GPA with string formatting.

---
+++ ClassesLibrary/Student.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassesLibrary
+{
+    public class Student
+    {
+        #region Fields
+        private string _firstName;
+        private string _lastName;
+        private string _id;
+        private float _gpa;
+        #endregion
+
+        #region Properties
+        public string FirstName
+        {
+            get { return _firstName; }
+            set { _firstName = value; }
+        }//end property FirstName
+
+        public string LastName
+        {
+            get { return _lastName; }
+            set { _lastName = value; }
+        }//end property LastName
+
+        public string ID
+        {
+            get { return _id; }
+            set { _id = value; }
+        }//end property ID
+
+        public float GPA
+        {
+            get { return _gpa; }
+            set { _gpa = value; }
+        }//end property GPA
+        #endregion
+
+        #region Constructors
+        public Student()
+        {
+
+        }//end Parameterless Constructor
+
+        public Student(string firstName, string lastName, string id, float gpa)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            ID = id;
+            GPA = gpa;
+        }//end FQCTR
+        #endregion
+
+        #region Methods
+        public override string ToString()
+        {
+            return string.Format("First Name: {0}" +
+                "\nLast Name: {1}" +
+                "\nID: {2}" +
+                "\nGPA: {3:n2}",
+                FirstName,
+                LastName,
+                ID,
+                GPA);
+        }//end override ToString()
+        #endregion
+    }//end class
+}//end namespace
--- TesterProgram/Program.cs
+++ TesterProgram/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ClassesLibrary;
 
 namespace TesterProgram
 {
@@ -10,6 +11,25 @@ namespace TesterProgram
     {
         static void Main(string[] args)
         {
+            Console.Title = "--== CSF2 Homework - Data Type Classes ==--";
+
+            #region Student
+            Console.WriteLine("\n-=-= Student =-=-\n");
+
+            Student s1 = new Student();
+            s1.FirstName = "Jerry";
+            s1.LastName = "Greensdale";
+            s1.ID = "jgreensdale";
+            s1.GPA = 3.66f;
+            Console.WriteLine(s1);
+
+            Console.WriteLine();
+
+            Student s2 = new Student("John", "Doe", "jdoe", 3.52f);
+            Console.WriteLine(s2);
+            #endregion
+
+
         }//end Main()
     }//end class
 }//end namespace
